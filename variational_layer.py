063104fbb8d28af0800a97966499c44e63ec8d05
AlanLahoud <lahoudalan93@gmail.com>
2022-08-05 20:34:14 +0200

    Variational Layer moved to another file, understood the importance of K in the illustrative example. Low K will give lower variance on predictions

---
+++ variational_layer.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+
+class VariationalLayer(nn.Module):
+    def __init__(self, 
+                 input_size, output_size,
+                 prior_mu, prior_rho,
+                 n_samples
+                ):
+        super().__init__()
+        
+        # Bias weight
+        input_size = input_size + 1
+        
+        # Defining Prior distribution (Gaussian)
+        self.prior_mu = torch.tensor(prior_mu)
+        self.prior_rho = torch.tensor(prior_rho)
+        
+        # Defining Variational class (Gaussian class)
+        self.theta_mu = nn.Parameter(
+            torch.Tensor(input_size, output_size).uniform_(
+                -0.5, 0.5)).float()
+        self.theta_rho = nn.Parameter(
+            torch.Tensor(input_size, output_size).uniform_(
+                -5,-4)).float()
+        
+        # Defining some constants
+        self.logsqrttwopi = torch.log(
+            torch.sqrt(2*torch.tensor(torch.pi)))
+        self.K = torch.tensor(1)
+        
+        # Defining number of samples for forward
+        self.n_samples = n_samples
+    
+    def rho_to_sigma(self, theta_rho):
+        return torch.log(1 + torch.exp(theta_rho))
+
+    def sample_weight(self, theta_mu, theta_rho):
+        w = (theta_mu 
+        + self.rho_to_sigma(theta_rho)*torch.randn(
+            (self.n_samples, theta_mu.shape[0], theta_mu.shape[1])
+        ))
+        return w
+
+    def log_prob_gaussian(self, x, mu, rho):
+            return (
+                - self.logsqrttwopi
+                - torch.log(self.rho_to_sigma(rho))
+                - ((x - mu)**2)/(2*self.rho_to_sigma(rho)**2)
+            ).sum(axis=[1, 2]).mean()
+    
+    def prior(self, w):
+        return self.log_prob_gaussian(
+            w, self.prior_mu, self.prior_rho)
+        
+    def variational(self, w, theta_mu, theta_rho):
+        return self.log_prob_gaussian(
+            w, theta_mu, theta_rho) 
+    
+    def kl_divergence_layer(self):
+        theta_mu = self.theta_mu
+        theta_rho = self.theta_rho
+        w = self.sample_weight(theta_mu, theta_rho)
+        Q = self.variational(w, theta_mu, theta_rho)
+        P = self.prior(w)
+        KL = Q - P 
+        return KL
+    
+    def forward(self, x_layer):
+        theta_mu = self.theta_mu
+        theta_rho = self.theta_rho
+        w = self.sample_weight(theta_mu, theta_rho)    
+        x_next_layer = torch.bmm(x_layer, w[:, :-1, :]) + w[:,-1,:].unsqueeze(1)
+        return x_next_layer
