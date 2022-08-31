data normalized, multimodel, training fixed with mse average

classic_newsvendor.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import train_test_split
 
 # Utils
 import data_generator
-from model import VariationalLayer, VariationalNet, StandardNet
+from model import VariationalLayer, VariationalNet, StandardNet, VariationalNet2
 
 from train import TrainDecoupled
 
@@ -60,9 +60,9 @@ else:
 # Setting parameters (change if necessary)
 N = 8000 # Total data size
 N_train = 5000 # Training data size
-N_SAMPLES = 16 # Sampling size while training
-BATCH_SIZE_LOADER = 32 # Standard batch size
-EPOCHS = 100 
+N_SAMPLES = 8 # Sampling size while training
+BATCH_SIZE_LOADER = 64 # Standard batch size
+EPOCHS = 350 
 noise_type = 'multimodal'
 
 # Data manipulation
@@ -96,15 +96,15 @@ else:
     h = StandardNet(input_size, output_size, eps).to(dev)
     K = 0
     
-opt_h = torch.optim.Adam(h.parameters(), lr=0.005)
-mse_loss_mean = nn.MSELoss(reduction='mean')
+opt_h = torch.optim.Adam(h.parameters(), lr=0.001)
+mse_loss = nn.MSELoss(reduction='none')
 
 # Training regression with ELBO or MSE
 train_elbo = TrainDecoupled(
                 bnn = bnn,
                 model=h,
                 opt=opt_h,
-                loss_data=mse_loss_mean,
+                loss_data=mse_loss,
                 K=K,
                 training_loader=training_loader,
                 validation_loader=validation_loader
@@ -116,7 +116,7 @@ train_elbo.train(EPOCHS=EPOCHS)
 if bnn:
     Kstr = str(K).replace('.','')
     plvstr = str(PLV).replace('.','')
-    model_name = f'elbo_nv1_{Kstr}_{plvstr}'
+    model_name = f'elbo2_nv1_{Kstr}_{plvstr}'
 else:
     epstr = str(eps).replace('.','')
     model_name = f'mse_nv1_{epstr}'

data_generator.py
@@ -1,5 +1,8 @@
 import numpy as np
 import torch
+from sklearn import preprocessing
+
+
 
 class ArtificialDataset(torch.utils.data.Dataset):
     def __init__(self, X, y):
@@ -36,11 +39,9 @@ def data_1to1(N, xl=-1, xh=1, noise_level=1, noise_type='gaussian'):
     else:
         print('noise_type not considered')
         exit()
-        
     
-    
-    X = np.expand_dims(X, axis=1)
-    y = np.expand_dims(y, axis=1)
+    X = np.expand_dims(preprocessing.scale(X), axis=1)
+    y = np.expand_dims(preprocessing.scale(y), axis=1)
     
     return X, y
 

model.py
@@ -22,7 +22,7 @@ class VariationalLayer(nn.Module):
                 -0.5, 0.5)).float()
         self.theta_rho = nn.Parameter(
             torch.Tensor(input_size, output_size).uniform_(
-                -5,-4)).float()
+                -4,-3)).float()
         
         # Defining some constants
         self.logsqrttwopi = torch.log(
@@ -81,13 +81,16 @@ class VariationalNet(nn.Module):
         self.n_samples = n_samples
         self.act1 = nn.ReLU()
         # Hidden layer sizes, if you add a layer you have to modify the code below
-        hl_sizes = [128, 64] 
+        hl_sizes = [32, 16] 
         self.linear1 = VariationalLayer(input_size, hl_sizes[0], 0, plv, n_samples)
         self.linear2 = VariationalLayer(hl_sizes[0], hl_sizes[1], 0, plv, n_samples)
-        self.linear3 = VariationalLayer(hl_sizes[1], output_size, 0, plv, n_samples)
+        self.linear3 = VariationalLayer(hl_sizes[1], hl_sizes[1], 0, plv, n_samples)
+        self.linear4 = VariationalLayer(hl_sizes[1], hl_sizes[1], 0, plv, n_samples)
+        self.linear5 = VariationalLayer(hl_sizes[1], output_size, 0, plv, n_samples)
         self.neurons = (
             (input_size+1)*hl_sizes[0] 
             + (hl_sizes[0]+1)*hl_sizes[1] 
+            + 2*(hl_sizes[1]+1)*hl_sizes[1] 
             + (hl_sizes[1]+1)*output_size
         )
     
@@ -99,6 +102,10 @@ class VariationalNet(nn.Module):
         x = self.linear2(x)
         x = self.act1(x)
         x = self.linear3(x)
+        x = self.act1(x)
+        x = self.linear4(x)
+        x = self.act1(x)
+        x = self.linear5(x)
         return x
     
     def forward_dist(self, x):
@@ -109,6 +116,8 @@ class VariationalNet(nn.Module):
             self.linear1.kl_divergence_layer() 
             + self.linear2.kl_divergence_layer()
             + self.linear3.kl_divergence_layer()
+            + self.linear4.kl_divergence_layer()
+            + self.linear5.kl_divergence_layer()
         )/self.neurons
         return kl
     
@@ -117,6 +126,80 @@ class VariationalNet(nn.Module):
         self.linear1.n_samples = n_samples
         self.linear2.n_samples = n_samples
         self.linear3.n_samples = n_samples
+        self.linear4.n_samples = n_samples
+        self.linear5.n_samples = n_samples
+        
+        
+class VariationalNet2(nn.Module):
+    def __init__(self, n_samples, input_size, output_size, plv):
+        super().__init__()
+        self.output_type_dist = True
+        self.n_samples = n_samples
+        self.act1 = nn.ReLU()
+        self.act2 = nn.Tanh()
+        # Hidden layer sizes, if you add a layer you have to modify the code below
+        hl_sizes = [64, 32] 
+        self.linear1 = VariationalLayer(input_size, hl_sizes[0], 0, plv, n_samples)
+        self.linear2 = VariationalLayer(hl_sizes[0], hl_sizes[0], 0, plv, n_samples)
+        self.linear3 = VariationalLayer(hl_sizes[0], hl_sizes[0], 0, plv, n_samples)
+        self.linear4 = VariationalLayer(hl_sizes[0], hl_sizes[1], 0, plv, n_samples)
+        self.linear5A = VariationalLayer(hl_sizes[1], output_size, 0, plv, n_samples)
+        self.linear5B = VariationalLayer(hl_sizes[1], output_size, 0, 2*plv, n_samples)
+        self.linear5C = VariationalLayer(hl_sizes[1], output_size, 0, 4*plv, n_samples)
+        self.linear5D = VariationalLayer(hl_sizes[1], output_size, 0, 8*plv, n_samples)
+        self.neurons = (
+            (input_size+1)*hl_sizes[0] 
+            + 2*(hl_sizes[0]+1)*hl_sizes[1]
+            + (hl_sizes[0]+1)*hl_sizes[1] 
+            + (hl_sizes[0]+1)*hl_sizes[1] 
+            + (hl_sizes[1]+1)*output_size
+            + (hl_sizes[1]+1)*output_size
+        )
+    
+    def forward(self, x):
+        x = torch.unsqueeze(x, 0)
+        x = x.expand((self.n_samples, x.shape[1], x.shape[2]))
+        x = self.linear1(x)
+        x = self.act2(x)
+        x = self.linear2(x)
+        x = self.act1(x)
+        x = self.linear3(x)
+        x = self.act1(x)
+        x = self.linear4(x)
+        x = self.act1(x)
+        xA = self.linear5A(x)
+        xB = self.linear5B(x)
+        xC = self.linear5C(x)
+        xD = self.linear5D(x)
+        y = torch.vstack((xA, xB, xC, xD))
+        return y
+    
+    def forward_dist(self, x):
+        return self(x)
+    
+    def kl_divergence_NN(self):
+        kl = (
+            self.linear1.kl_divergence_layer() 
+            + self.linear2.kl_divergence_layer()
+            + self.linear3.kl_divergence_layer()
+            + self.linear4.kl_divergence_layer()
+            + self.linear5A.kl_divergence_layer()
+            + self.linear5B.kl_divergence_layer()
+            + self.linear5C.kl_divergence_layer()
+            + self.linear5D.kl_divergence_layer()
+        )/self.neurons
+        return kl
+    
+    def update_n_samples(self, n_samples):
+        self.n_samples = n_samples
+        self.linear1.n_samples = n_samples
+        self.linear2.n_samples = n_samples
+        self.linear3.n_samples = n_samples
+        self.linear4.n_samples = n_samples
+        self.linear5A.n_samples = n_samples
+        self.linear5B.n_samples = n_samples
+        self.linear5C.n_samples = n_samples
+        self.linear5D.n_samples = n_samples
         
     
 class StandardNet(nn.Module):
@@ -151,9 +234,8 @@ class StandardNet(nn.Module):
         return y
         
         
-        
-        
-        
+       
+   
         
         
         

train.py
@@ -1,6 +1,5 @@
 import torch
 
-
 class TrainDecoupled():
     
     def __init__(self, bnn, model, opt, loss_data, K, training_loader, validation_loader):
@@ -11,6 +10,10 @@ class TrainDecoupled():
         self.training_loader = training_loader
         self.validation_loader = validation_loader
         self.bnn = bnn
+        
+        self.logsqrttwopi = torch.log(
+            torch.sqrt(2*torch.tensor(torch.pi)))
+      
     
     def train_one_epoch(self):
         data_running_loss = 0.
@@ -18,7 +21,7 @@ class TrainDecoupled():
 
         n = len(self.training_loader.dataset)
         n_batches = len(self.training_loader)
-        
+                
         for i, data in enumerate(self.training_loader):
 
             x_batch, y_batch = data
@@ -26,15 +29,22 @@ class TrainDecoupled():
  
             y_preds = self.model(x_batch)
     
-            if self.bnn:
-                y_preds = y_preds.mean(axis=0)
 
-            loss_data_ = self.loss_data(y_preds, y_batch)
-            
             if self.bnn:
+                #y_preds = y_preds.mean(axis=0)
+                y_batch = y_batch.unsqueeze(0).expand(y_preds.shape)
+                loss_data_ = self.loss_data(y_preds, y_batch)
+                loss_data_ = loss_data_.mean(axis=1) #through batch
+                loss_data_ = loss_data_.mean(axis=0) #through stochastic weights
                 kl_loss_ = self.K*self.model.kl_divergence_NN()/n_batches
+
             else:
+                y_batch = y_batch.unsqueeze(0).expand(y_preds.shape)
+                loss_data_ = self.loss_data(y_preds, y_batch)
+                loss_data_ = loss_data_.mean(axis=0) #through batch
                 kl_loss_ = torch.tensor(0)
+                
+            loss_data_ = loss_data_.mean(axis=-1) #through output dimension
 
             total_loss = loss_data_ + kl_loss_
             total_loss.backward()
@@ -78,10 +88,18 @@ class TrainDecoupled():
 
                 y_val_preds = self.model(x_val_batch)
                 
-                if self.bnn:
-                    y_val_preds = y_val_preds.mean(axis=0)
-
+                y_val_batch = y_val_batch.unsqueeze(0).expand(y_val_preds.shape)
                 loss_data_ = self.loss_data(y_val_preds, y_val_batch)
+                
+                if self.bnn:
+                    loss_data_ = loss_data_.mean(axis=1) #through batch
+                    loss_data_ = loss_data_.mean(axis=0) #through stochastic weights
+                    
+                else:
+                    loss_data_ = loss_data_.mean(axis=0) #through batch
+                
+                loss_data_ = loss_data_.mean(axis=-1) #through output dimension
+
                 loss_data_running_loss_v += loss_data_
 
             avg_vloss_data = (loss_data_running_loss_v/n_batches).item()
@@ -92,7 +110,7 @@ class TrainDecoupled():
             print('DATA LOSS \t train {} valid {}'.format(
                 round(avg_loss_data_loss, 2), round(avg_vloss_data, 2)))
             print('KL LOSS \t train {} valid {}'.format(
-                round(avg_kl_loss, 2), round(avg_vklloss, 2)))
+                round(avg_kl_loss/(self.K+0.000001), 2), round(avg_vklloss/(self.K+0.000001), 2)))
             print('ELBO LOSS \t train {} valid {}'.format(
                 round(avg_loss, 2), round(avg_vloss, 2)))
 