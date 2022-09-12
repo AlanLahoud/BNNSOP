5601d764afe083a9e9218eab3e68b57e35be2b26
AlanLahoud <lahoudalan93@gmail.com>
2022-09-12 09:20:29 +0200

    start coding constrained newsvendor problem, creating deterministic params file generator

--- params_newsvendor.py
+++ params_newsvendor.py
@@ -6,7 +6,7 @@ import torch
 ## OP deterministic parameters ##################################################
 #################################################################################
 
-def get_params(n_items, is_discrete, q_factor, seed_number):
+def get_params(n_items, seed_number):
     
     np.random.seed(seed_number)
     
@@ -14,6 +14,11 @@ def get_params(n_items, is_discrete, q_factor, seed_number):
     params_list = []
     
     for i in range(0, n_items):
+        
+        q = 40 + np.random.randint(-30, 30) # Fixed cost for each item
+        qs = 180 + np.random.randint(-80, 80)  # Shortage cost fpr each item
+        qw = 120 + np.random.randint(-50, 50) # Excess cost for each item
+        
         c = 60 + np.random.randint(-30, 30) # Fixed cost for each item
         cs = 180 + np.random.randint(-80, 80)  # Shortage cost fpr each item
         cw = 120 + np.random.randint(-50, 50) # Excess cost for each item
@@ -22,16 +27,12 @@ def get_params(n_items, is_discrete, q_factor, seed_number):
         pr = int(100000/(c + cs + cw) + np.random.randint(-50, 50))
         si = int(100000/(c + cs + cw) + np.random.randint(-50, 50))
 
-        params_list.append([c, cs, cw, pr, si])
+        params_list.append([q, qs, qw, c, cs, cw, pr, si])
 
     df_parameters = pd.DataFrame(data=params_list, columns = params_name)
 
     # Generate a bound for inequalities of Budget and Size
-    avg_sales = 0
-    if is_discrete:
-        avg_sales = 1.3
-    else:
-        avg_sales = 30 
+    avg_sales = 5 
     B = 400*avg_sales*n_items*np.random.uniform(0.3, 0.6)
     S = 400*avg_sales*n_items*np.random.uniform(0.3, 0.6)
     
@@ -39,9 +40,9 @@ def get_params(n_items, is_discrete, q_factor, seed_number):
     # Building the parameters as numpy and torch dictionary
     params = {}
     
-    params['q'] = (q_factor*df_parameters['c']).tolist()
-    params['qs'] = (q_factor*df_parameters['cs']).tolist()
-    params['qw'] = (q_factor*df_parameters['cw']).tolist()
+    params['q'] = df_parameters['c'].tolist()
+    params['qs'] = df_parameters['qs'].tolist()
+    params['qw'] = df_parameters['qw'].tolist()
     
     params['c'] = df_parameters['c'].tolist()
     params['cs'] = df_parameters['cs'].tolist()
