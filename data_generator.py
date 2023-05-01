cd33208c8fcce42912f6adc3084773fe0291bb1f
AlanLahoud <lahoudalan93@gmail.com>
2023-05-01 19:24:31 +0200

    fixing noise

--- data_generator.py
+++ data_generator.py
@@ -189,8 +189,10 @@ def gen_data(N, n_assets, nl, seed_number=42, samples_dist=1):
         for i in range(1, n_assets + 1):
             interm = gen_intermediate(i, n_assets, x1, x2, x3)
             Y[:,i-1] = (np.sin(interm) - np.sin(interm).mean())
-            
-            Y[:,i-1] = 0.2 + Y[:,i-1] - np.abs(nl*np.abs(x1)*(np.random.exponential(1, size=Y[:,0].shape)))
+
+            #Y[:,i-1] = 0.7 + Y[:,i-1] - np.abs(nl*np.abs(x1 + x2)*(np.random.exponential(1, size=Y[:,0].shape)))
+                        
+            Y[:,i-1] = 0.2 + Y[:,i-1] - np.abs(nl*np.abs(x1)*(np.random.lognormal(0, 1, size=Y[:,0].shape)))
                         
         return Y
         
