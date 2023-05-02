10a36b537141a2162d4c5cb92f7b88c51744a9e8
AlanLahoud <lahoudalan93@gmail.com>
2023-05-02 18:25:33 +0200

    noise multimodal

--- data_generator.py
+++ data_generator.py
@@ -188,7 +188,7 @@ def gen_data(N, n_assets, nl, seed_number=42, samples_dist=1):
         Y = np.zeros((N, n_assets))
         for i in range(1, n_assets + 1):
             interm = gen_intermediate(i, n_assets, x1, x2, x3)
-            Y[:,i-1] = np.sin(interm)
+            Y[:,i-1] = (np.sin(interm) - np.sin(interm).mean())
 
             #Y[:,i-1] = 0.7 + Y[:,i-1] - np.abs(nl*np.abs(x1 + x2)*(np.random.exponential(1, size=Y[:,0].shape)))
             
@@ -196,13 +196,13 @@ def gen_data(N, n_assets, nl, seed_number=42, samples_dist=1):
             szh = (Y[:,0].shape[0])//2
         
             noise = np.hstack(
-                (np.random.normal(-2, 2, szh), 
+                (np.random.normal(-2, 3, szh), 
                  np.random.normal(2, 1, sz - szh))
             )
             
             np.random.shuffle(noise)
                         
-            Y[:,i-1] = Y[:,i-1] + 0.5 + nl*np.abs(x1 + x2)*(noise)
+            Y[:,i-1] = 0.1 + Y[:,i-1] + nl*np.abs(x1 + x2)*(noise)
                     
         return Y
         
