17bdb5a0f4feb9e205315deadd934978a8bed757
AlanLahoud <lahoudalan93@gmail.com>
2023-05-02 13:18:24 +0200

    variable neurons model

--- minmaxportfolio.py
+++ minmaxportfolio.py
@@ -8,7 +8,7 @@ import random
 import sys
 
 import data_generator
-from model import StrongStandardNet, StrongVariationalNet
+from model import VariableStandardNet, VariableVariationalNet
 from train import TrainDecoupled, TrainCombined
 
 from sklearn.preprocessing import StandardScaler
@@ -149,7 +149,7 @@ def run_minimax_op(
     
     
     if method_name == 'bnn':
-        h = StrongVariationalNet(
+        h = VariableVariationalNet(
         n_samples=N_SAMPLES,
         input_size=X.shape[1], 
         output_size=Y.shape[1], 
@@ -160,7 +160,7 @@ def run_minimax_op(
     
     #ANN Baseline model
     elif method_name == 'ann':
-        h = StrongStandardNet(X.shape[1], Y.shape[1]).to(dev)
+        h = VariableStandardNet(X.shape[1], Y.shape[1]).to(dev)
         K = 0 # There is no K in ANN
         N_SAMPLES = 1
 
