a1e4b5bc505cf3eeede85be19cd918836a539a23
Alan Lahoud <alan.lahoud@oru.se>
2024-06-05 10:31:25 +0200

    pop and pop2

---
+++ data_generator.py
@@ -0,0 +1,305 @@
+import numpy as np
+import pandas as pd
+import torch
+from sklearn import preprocessing
+
+class ArtificialDataset(torch.utils.data.Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        return
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        X_i = self.X[idx]
+        y_i = self.y[idx]
+        return X_i, y_i
+    
+class ArtificialNoisyDataset(torch.utils.data.Dataset):
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+        return
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        X_i = self.X[idx]
+        Y_i = self.Y[:,idx,:]
+        return X_i, Y_i
+    
+    
+class ArtificialDistDataset(torch.utils.data.Dataset):
+    def __init__(self, X, Y, Ydist):
+        self.X = X
+        self.Y = Y
+        self.Ydist = Ydist
+        return
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        X_i = self.X[idx]
+        Y_i = self.Y[idx]
+        Ydist_i = self.Ydist[:,idx,:]
+        return X_i, Y_i, Ydist_i
+
+
+# Creating a nonlinear (x, y) data
+# with nonlinear relation and noise
+def data_1to1(N, noise_level=1, seed_number=42, 
+              noise_type='gaussian', 
+              uniform_input_space=False, 
+              add_yfair=False):
+    
+    np.random.seed(seed_number)
+    if uniform_input_space:
+        X = np.arange(-4, 4, 8/N)
+    else:
+        X = np.hstack((np.random.normal(-3, 1, N//2), 
+                       np.random.normal(3, 1, N-N//2)))
+    
+    def gen_output(X):
+        
+        # Nonlinear relation between X and Y
+        y_perfect = 5 + np.abs(
+            np.abs(6*X)*np.sin(X) + np.sin(6*X)*(0.5*X))
+        
+        # Input dependent Noise
+        if noise_type == 'gaussian':
+            n = np.where(X>3, 0, np.random.normal(0, noise_level, N))
+            y = y_perfect + n*np.sin(X)*np.abs(X)*2
+        elif noise_type == 'multimodal':
+            n = np.hstack(
+                (np.random.normal(6, noise_level, N//4), 
+                 np.random.normal(2, noise_level, N-N//4))
+            )
+            np.random.shuffle(n)
+            n = np.where(X<3, n, 0)
+            y = y_perfect + 0.5*n*np.sin(X)*np.abs(X)*2    
+        elif noise_type == 'poisson':
+            n = np.random.poisson(lam=5*noise_level, size=N)    
+            np.random.shuffle(n)
+            n = np.where(X<3, n, 0)
+            y = y_perfect + 0.5*n*np.sin(X)*np.abs(X)*2 
+        elif noise_type == 'exponential':
+            n = np.random.exponential(noise_level, size=N)  
+            np.random.shuffle(n)
+            n = np.where(X<3, n, 0)
+            y = np.clip(y_perfect - 0.5*n*np.abs(X)**2, 0.1, None)
+            
+            
+            
+        else:
+            print('noise_type not considered')
+            exit()    
+        y = np.where(y<0, 0, y)
+        y = np.expand_dims(y, axis=1)
+        return y
+    
+    y = gen_output(X)
+    
+    if add_yfair:
+        y_noisy = np.zeros((10000, N, 1))
+        for i in range(0, 10000):
+            y_noisy[i,:,:] = gen_output(X)
+        y_noisy = torch.tensor(y_noisy, dtype=torch.float32)
+    else:
+        y_noisy = y
+    
+    X = np.expand_dims(X, axis=1)
+    
+    return X, y, y_noisy
+
+
+# Creating a nonlinear (X, Y) data
+# with nonlinear relation and noise
+def data_4to8(N, noise_level=1, seed_number=42, 
+              uniform_input_space=False, 
+              add_yfair=False):
+
+    np.random.seed(seed_number)
+    
+    if uniform_input_space:
+        x1 = np.arange(-4, 4, 8/N)
+        x2 = np.arange(-5, 5, 10/N)
+        x3 = np.arange(-4, 4, 8/N)
+        x4 = np.arange(-4, 3, 7/N)
+    else:
+        x1 = np.hstack((np.random.normal(-3, 1, N//2), 
+                        np.random.normal(3, 1, N-N//2)))
+        x2 = np.hstack((np.random.normal(-4, 1, N//2), 
+                        np.random.normal(4, 1, N-N//2)))
+        x3 = np.hstack((np.random.normal(-3, 0.7, N//2), 
+                        np.random.normal(3, 0.7, N-N//2)))
+        x4 = np.hstack((np.random.normal(-3, 1, N//2), 
+                        np.random.normal(1, 2, N-N//2)))
+    
+    def gen_output(x1, x2, x3, x4):
+        y1_perfect = np.maximum(
+            10 + np.abs(x1)*np.sin(x2) + 4*np.sin(6*x3), 0)
+        y2_perfect = np.maximum(
+            3 + np.abs(10*x2)*np.sin(x3)**2 + 2*np.sin(6*x1), 0)
+        y3_perfect = np.maximum(
+            10 + np.abs(4*x3)**0.5*np.sin(x2) + 4*np.sin(2*x4), 0)
+        y4_perfect = np.maximum(
+            7 + np.abs(6*x4)*np.sin(x1) + 2*np.sin(6*x2)**2, 0)
+
+        y5_perfect = 5 + y1_perfect + y2_perfect
+        y6_perfect = 5 + y2_perfect + y3_perfect
+
+        n_gaussian_1 = np.random.normal(1, noise_level, N)
+        n_gaussian_2 = np.random.normal(1, 0.5*noise_level, N)
+        n_multimodal_1 = np.hstack(
+                (np.random.normal(0.5, noise_level, N//4), 
+                 np.random.normal(2, noise_level, N-N//4))
+            )
+        n_multimodal_2 = np.hstack(
+                (np.random.normal(0.5, 0.5*noise_level, N//4), 
+                 np.random.normal(2, noise_level, N-N//4))
+            )
+
+        n_poisson_1 = np.random.poisson(
+            lam=0.2*noise_level, size=N)
+        n_poisson_2 = np.random.poisson(
+            lam=0.4*noise_level, size=N)
+
+
+        y1 = np.maximum(y1_perfect + n_gaussian_1, 0)
+        y2 = np.maximum(y2_perfect + n_gaussian_2, 0)
+        y3 = np.maximum(y3_perfect + n_multimodal_2, 0)
+        y4 = np.maximum(y4_perfect + n_poisson_1, 0)
+
+        y5 = np.maximum(y5_perfect + n_multimodal_1, 0)
+        y6 = np.maximum(y6_perfect + n_gaussian_1, 0)
+
+        Y = np.vstack((y1, y2, y3, y4, y5, y6)).T.round(3)
+        return Y
+    
+    Y = gen_output(x1, x2, x3, x4)
+
+    np.random.seed(0)
+    if add_yfair:
+        Y_noisy = np.zeros((32, N, 6))
+        for i in range(0, 32):
+            Y_noisy[i,:,:] = gen_output(x1, x2, x3, x4)
+        Y_noisy = torch.tensor(Y_noisy, dtype=torch.float32)
+    else:
+        Y_noisy = Y
+
+    X = np.vstack((x1, x2, x3, x4)).T.round(3)
+    
+    return X, Y, Y_noisy
+
+
+
+
+# Data for portfolio risk optimization (minimax)
+def gen_intermediate(num, n_assets, x1, x2, x3):      
+        factor = num * 2/(n_assets)
+        return x1**factor + x2**factor + x3**factor
+    
+def gen_data(N, n_assets, nl, seed_number=42, samples_dist=1):
+    np.random.seed(seed_number)
+    x1 = np.random.normal(1, 1, size = N).clip(0)
+    x2 = np.random.normal(1, 1, size = N).clip(0)
+    x3 = np.random.normal(1, 1, size = N).clip(0)
+    X = np.vstack((x1, x2, x3)).T
+    
+    def geny(x1, x2, x3, N, n_assets):
+        Y = np.zeros((N, n_assets))
+        for i in range(1, n_assets + 1):
+            interm = gen_intermediate(i, n_assets, x1, x2, x3)
+            Y[:,i-1] = (np.sin(interm) - np.sin(interm).mean())
+
+            #Y[:,i-1] = 0.7 + Y[:,i-1] - np.abs(nl*np.abs(x1 + x2)*(np.random.exponential(1, size=Y[:,0].shape)))
+            
+            sz = Y[:,0].shape[0]
+            szh = (Y[:,0].shape[0])//2
+        
+            noise = np.hstack(
+                (np.random.normal(-4, 4, szh), 
+                 np.random.normal(4, 1, sz - szh))
+            )
+            
+            np.random.shuffle(noise)
+                        
+            Y[:,i-1] = 0.3 + Y[:,i-1] + nl*(noise)
+                    
+        return Y
+        
+    Y = geny(x1, x2, x3, N, n_assets)
+        
+    Y_dist = np.zeros((samples_dist, N, n_assets))
+    for i in range(0, samples_dist):
+        Y_dist[i, :, :] = geny(x1, x2, x3, N, n_assets)
+        
+    return X, Y, Y_dist
+
+def gen_cond_dist(N, n_assets, n_samples, nl, seed_number=420):
+    np.random.seed(seed_number)
+    Y_dist = np.zeros((n_samples, N, n_assets))
+    for i in range(0, n_samples):
+        Y_dist[i, :, :] = gen_data(N, n_assets, nl, seed_number=np.random.randint(0,999999))[1]
+    return Y_dist
+
+
+
+def gen_processed_stocks():
+
+    def process_stocks(dfs):
+
+        feat_technical = ['Volume', 'mom1', 'mom2', 'mom3',
+                         'ROC_5','ROC_10', 'ROC_15', 'ROC_20',
+                         'EMA_10', 'EMA_20', 'EMA_50', 'EMA_200']
+
+        df_total = pd.DataFrame()
+        Y = np.zeros((dfs[0].shape[0], len(dfs)))
+        for i, df in enumerate(dfs):
+            df_total = pd.concat([df_total, df[feat_technical]], axis=1)
+            Y[:,i] = df['Close'].pct_change().fillna(0)
+
+
+        feat_common = ['DTB4WK', 'DTB3', 'DTB6', 'DGS5', 'DGS10', 'Oil', 'Gold', 'DAAA',
+                        'DBAA', 'GBP', 'JPY', 'CAD', 'CNY', 'AAPL', 'AMZN', 'GE', 'JNJ', 'JPM',
+                        'MSFT', 'WFC', 'XOM', 'FCHI', 'FTSE', 'GDAXI', 'GSPC', 'HSI', 'IXIC',
+                        'SSEC', 'RUT', 'NYSE', 'TE1', 'TE2', 'TE3', 'TE5', 'TE6', 'DE1', 'DE2',
+                        'DE4', 'DE5', 'DE6', 'CTB3M', 'CTB6M', 'CTB1Y', 'AUD', 'Brent',
+                        'CAC-F', 'copper-F', 'WIT-oil', 'DAX-F', 'DJI-F', 'EUR', 'FTSE-F',
+                        'gold-F', 'HSI-F', 'KOSPI-F', 'NASDAQ-F', 'GAS-F', 'Nikkei-F', 'NZD',
+                        'silver-F', 'RUSSELL-F', 'S&P-F', 'CHF', 'Dollar index-F',
+                        'Dollar index', 'wheat-F', 'XAG', 'XAU']
+
+        df_total = pd.concat([df_total, dfs[0][feat_common]], axis=1)
+
+        X = np.array(df_total.fillna(0))
+
+        return X, Y
+
+    df_dji = pd.read_csv('./data_stocks/Processed_DJI.csv')
+    df_nas = pd.read_csv('./data_stocks/Processed_NASDAQ.csv')
+    df_nyse = pd.read_csv('./data_stocks/Processed_NYSE.csv')
+    df_rus = pd.read_csv('./data_stocks/Processed_RUSSELL.csv')
+    df_sp = pd.read_csv('./data_stocks/Processed_S&P.csv')
+
+    X, Y = process_stocks([df_dji, df_nas, df_nyse, df_rus, df_sp])
+    
+    corr = np.corrcoef(np.hstack((X, Y)).T)
+
+    a = list(np.argwhere((corr[-1,:]>0.3)).squeeze())
+    b = list(np.argwhere((corr[-2,:]>0.3)).squeeze())
+    c = list(np.argwhere((corr[-3,:]>0.3)).squeeze())
+    d = list(np.argwhere((corr[-4,:]>0.3)).squeeze())
+    e = list(np.argwhere((corr[-5,:]>0.3)).squeeze())
+
+    Xcols = list(set(list(np.arange(0,133))) - set.intersection(set(list(np.arange(0,133))), set(a + b + c + d + e)))
+    
+    X = X[:, Xcols]
+    
+    #Y = 1000*Y #to avoid problems in the approx solver
+    
+    return X, Y, None
---
+++ train.py
@@ -0,0 +1,365 @@
+import sys
+import torch
+import numpy as np
+import math
+from tqdm import tqdm
+import copy
+
+class TrainDecoupled():
+    """
+    Class to help on training process using the 
+    Decoupled Learning Approach (see paper).
+    """
+    
+    def __init__(self, bnn, model, opt, loss_data, K, 
+                 aleat_bool, training_loader, 
+                 validation_loader, dev, explr=0.99):
+        self.model = model # Neural network (ANN or BNN)
+        self.opt = opt
+        self.loss_data = loss_data # NLL loss
+        self.K = K # Useful only for BNN
+        self.aleat_bool = aleat_bool # True if modeling aleatoric uncer
+        self.training_loader = training_loader
+        self.validation_loader = validation_loader
+        self.bnn = bnn # True if BNN, False if ANN
+        self.dev = dev
+        self.scheduler = torch.optim.lr_scheduler.ExponentialLR(
+            opt, gamma=explr)
+        
+        self.logsqrttwopi = torch.log(
+            torch.sqrt(2*torch.tensor(math.pi)))
+      
+    
+    def train_one_epoch(self, flag_pretrain):
+        """
+        Update ANN or BNN weights with Decoupled Learning approach 
+        for one epoch. 
+        """
+        data_running_loss = 0.
+        kl_running_loss = 0.
+
+        n = len(self.training_loader.dataset)
+        n_batches = len(self.training_loader)
+        
+        if flag_pretrain:
+            bnn = self.bnn
+            aleat_bool = False
+        else:
+            bnn = self.bnn
+            aleat_bool = self.aleat_bool
+            
+                
+        for i, data in enumerate(self.training_loader):
+            
+            x_batch, y_batch = data
+            x_batch = x_batch.to(self.dev)
+            y_batch = y_batch.to(self.dev)
+            
+            self.opt.zero_grad()
+            y_preds, rho_preds = self.model(x_batch)
+
+            if bnn:
+                y_batch = y_batch.unsqueeze(0).expand(y_preds.shape)
+                if aleat_bool:
+                    loss_data_ = self.loss_data(
+                        y_preds, y_batch)*torch.exp(-rho_preds) + rho_preds
+                else:
+                    loss_data_ = self.loss_data(y_preds, y_batch)
+                loss_data_ = loss_data_.mean(axis=1) #through batch
+                loss_data_ = loss_data_.mean(axis=0) #through stochastic weights
+                kl_loss_ = self.K*self.model.kl_divergence_NN()/n_batches
+
+            else:
+                if aleat_bool:
+                    loss_data_ = self.loss_data(
+                        y_preds, y_batch)*torch.exp(-rho_preds) + rho_preds
+                else:
+                    loss_data_ = self.loss_data(y_preds, y_batch)
+                loss_data_ = loss_data_.mean(axis=0) #through batch
+                kl_loss_ = torch.tensor(0)
+                
+            loss_data_ = loss_data_.mean(axis=-1) #through output dimension
+            total_loss = loss_data_ + kl_loss_
+            total_loss.backward()
+
+            self.opt.step()
+
+            data_running_loss += loss_data_.item()
+            kl_running_loss += kl_loss_.item()
+
+        loss_data = data_running_loss/n_batches
+        kl = kl_running_loss
+
+        return loss_data, kl
+    
+    
+    def train(self, EPOCHS=150, pre_train=-1):
+        """
+        Update ANN or BNN weights with Decoupled Learning approach 
+        for EPOCHS epochs.
+        """
+        epoch_number = 0
+ 
+        avg_best_vloss = np.inf
+        best_model = copy.deepcopy(self.model)
+    
+        for epoch in tqdm(range(EPOCHS)):
+            
+            if epoch < pre_train:
+                bnn = self.bnn
+                aleat_bool = False
+                flag_pretrain = True
+            else:
+                bnn = self.bnn
+                aleat_bool = self.aleat_bool
+                flag_pretrain = False
+            
+            self.model.train(True)
+            avg_loss_data_loss, avg_kl_loss = self.train_one_epoch(flag_pretrain)
+            avg_loss = avg_loss_data_loss + avg_kl_loss
+
+            self.model.train(False)
+            loss_data_running_loss_v = 0.0
+
+            n = len(self.validation_loader.dataset)
+            n_batches = len(self.validation_loader)
+            
+            if bnn:
+                kl_loss_ = self.K*self.model.kl_divergence_NN()
+            else:
+                kl_loss_ = torch.tensor(0)
+
+            # Validation metrics
+            for i, vdata in enumerate(self.validation_loader):
+
+                x_val_batch, y_val_batch = vdata                
+                x_val_batch = x_val_batch.to(self.dev)
+                y_val_batch = y_val_batch.to(self.dev)
+                
+                y_val_preds, rho_val_preds = self.model(x_val_batch)
+                
+                if bnn:
+                    y_val_batch = y_val_batch.unsqueeze(0).expand(y_val_preds.shape)                    
+                    if aleat_bool:
+                        loss_data_ = self.loss_data(y_val_preds, y_val_batch)*torch.exp(-rho_val_preds) + rho_val_preds
+                    else:
+                        loss_data_ = self.loss_data(y_val_preds, y_val_batch)                                     
+                    loss_data_ = loss_data_.mean(axis=1) #through batch
+                    loss_data_ = loss_data_.mean(axis=0) #through stochastic weights
+                    
+                else:
+                    if aleat_bool:
+                        loss_data_ = self.loss_data(y_val_preds, y_val_batch)*torch.exp(-rho_val_preds) + rho_val_preds
+                    else:
+                        loss_data_ = self.loss_data(y_val_preds, y_val_batch)                        
+                    loss_data_ = loss_data_.mean() #through batch
+                
+                loss_data_ = loss_data_.mean(axis=-1) #through output dimension
+                loss_data_running_loss_v += loss_data_.detach()
+              
+            avg_vloss_data = (loss_data_running_loss_v/n_batches).item()
+            avg_vklloss = (kl_loss_).item()
+
+            avg_vloss = avg_vloss_data + avg_vklloss
+
+            if epoch_number == 0 or (epoch_number+1)%1 == 0:           
+                print('------------------EPOCH {}------------------'.format(
+                    epoch_number + 1))
+
+                print('DATA LOSS \t train {} valid {}'.format(
+                    round(avg_loss_data_loss, 3), round(avg_vloss_data, 3)))
+                print('KL LOSS \t train {} valid {}'.format(
+                    round(avg_kl_loss/(self.K+0.000001), 2), round(avg_vklloss/(self.K+0.000001), 2)))
+                print('ELBO LOSS \t train {} valid {}'.format(
+                    round(avg_loss, 2), round(avg_vloss, 2)))
+    
+            if  avg_vloss < avg_best_vloss:
+                avg_best_vloss = avg_vloss
+                best_model=copy.deepcopy(self.model)
+            
+            epoch_number += 1
+            self.scheduler.step()
+            
+        return best_model 
+            
+
+            
+class TrainCombined():
+    """
+    Class to help on training process using the 
+    Combined Learning Approach (see paper).
+    """
+    def __init__(self, bnn, model, opt, K, aleat_bool, 
+                 training_loader, scaler, validation_loader, 
+                 OP, dev, explr=0.99, bm_stop=True):
+        self.model = model # Neural network (ANN or BNN)
+        self.opt = opt
+        self.K = K # Useful only for BNN
+        self.aleat_bool = aleat_bool # True if allow noise modeling
+        self.training_loader = training_loader
+        self.scaler = scaler # To denormalize to solve the OP in the training process
+        self.scaler_mean = torch.tensor(self.scaler.mean_, device=dev)
+        self.scaler_std = torch.tensor(self.scaler.scale_, device=dev)
+        self.validation_loader = validation_loader
+        self.bnn = bnn # True if BNN, False if ANN
+        self.end_loss = OP.end_loss # OP cost function
+        self.end_loss_dist = OP.end_loss_dist # OP expect cost function
+        self.dev = dev
+        self.scheduler = torch.optim.lr_scheduler.ExponentialLR(
+            opt, gamma=explr)
+        self.bm_stop = bm_stop
+       
+
+    def inverse_transform(self, inp):
+        """
+        Denormalize the data to solve the OP
+        """
+        return inp*self.scaler_std + self.scaler_mean
+       
+    def train_one_epoch(self, flag_pretrain):
+        """
+        Update ANN or BNN weights with Combined Learning approach 
+        for one epoch. 
+        """
+        n = len(self.training_loader.dataset)
+        n_batches = len(self.training_loader)
+
+        end_total_loss = 0.
+        kl_running_loss = 0.
+        
+        if flag_pretrain:
+            bnn = self.bnn
+            aleat_bool = False
+        else:
+            bnn = self.bnn
+            aleat_bool = self.aleat_bool
+        
+        for i, data in enumerate(self.training_loader):
+            
+            x_batch, y_batch = data
+            x_batch = x_batch.to(self.dev)
+            y_batch = y_batch.to(self.dev)
+            
+            self.opt.zero_grad()
+ 
+            y_preds, rho_preds = self.model(x_batch)
+
+            if aleat_bool:
+                y_preds = y_preds + torch.sqrt(
+                    torch.exp(rho_preds))*torch.randn(
+                    y_preds.size(), device = self.dev)
+    
+            y_preds = self.inverse_transform(y_preds)
+            y_batch = self.inverse_transform(y_batch)
+            if bnn:
+                #End loss: Expected OP cost value based on pred distrib
+                end_loss_ = self.end_loss_dist(y_preds, y_batch)
+                kl_loss_ = self.K*self.model.kl_divergence_NN()/n_batches
+                total_loss = end_loss_ + kl_loss_
+                
+            else:
+                #End loss: OP cost value based on pred value
+                end_loss_ = self.end_loss(y_preds, y_batch)                
+                kl_loss_ = torch.tensor(0)
+                total_loss = end_loss_ + kl_loss_
+            
+            total_loss.backward()
+            self.opt.step()
+
+            end_total_loss += end_loss_.item()
+            kl_running_loss += kl_loss_.item()
+
+        end_total_loss = end_total_loss/n_batches
+        kl = kl_running_loss
+
+        return end_total_loss, kl
+    
+    
+    
+    def train(self, EPOCHS=150, pre_train=-1):
+        """
+        Update ANN or BNN weights with Combined Learning approach 
+        for EPOCHS epochs. 
+        """
+        epoch_number = 0
+        
+        best_loss = np.inf
+        best_model = copy.deepcopy(self.model)
+        
+        for epoch in tqdm(range(EPOCHS)):
+            
+            if epoch < pre_train:
+                bnn = self.bnn
+                aleat_bool = False
+                flag_pretrain = True
+            else:
+                bnn = self.bnn
+                aleat_bool = self.aleat_bool
+                flag_pretrain = False
+
+            self.model.train(True)
+            end_loss, kl_loss = self.train_one_epoch(flag_pretrain)
+            total_loss = end_loss + kl_loss
+
+            self.model.train(False)
+
+            n = len(self.validation_loader.dataset)
+            n_batches = len(self.validation_loader)
+            
+            total_running_loss_v = 0.0
+            
+            if bnn:
+                kl_loss_val = self.K*self.model.kl_divergence_NN()
+            else:
+                kl_loss_val = torch.tensor(0)
+            
+            # Validation metrics
+            for i, vdata in enumerate(self.validation_loader):
+
+                x_val_batch, y_val_batch = vdata
+                
+                x_val_batch = x_val_batch.to(self.dev)
+                y_val_batch = y_val_batch.to(self.dev)
+                      
+                y_val_preds, rho_val_preds = self.model(x_val_batch)
+
+                if aleat_bool:
+                    y_val_preds = y_val_preds + torch.sqrt(
+                        torch.exp(rho_val_preds))*torch.randn(
+                        y_val_preds.size(), device = self.dev)
+
+                y_val_preds = self.inverse_transform(y_val_preds)
+                y_val_batch = self.inverse_transform(y_val_batch)
+                if bnn:
+                    total_loss_v = self.end_loss_dist(y_val_preds, y_val_batch)
+
+                else:
+                    total_loss_v = self.end_loss(y_val_preds, y_val_batch)
+                
+                total_running_loss_v += total_loss_v.detach().item()
+                                  
+            
+            end_loss_val = (total_running_loss_v/n_batches)
+            kl_loss_val = (kl_loss_val).item()
+            total_loss_val = end_loss_val + kl_loss_val
+            
+            if epoch_number == 0 or (epoch_number+1)%1 == 0: 
+                print('------------------EPOCH {}------------------'.format(
+                    epoch_number + 1))
+                print(
+                    f'END LOSS \t train {round(end_loss, 3)} valid {round(end_loss_val, 3)} \n',
+                    f'KL LOSS \t train {round(kl_loss/(self.K+0.0001), 3)} valid {round(kl_loss_val/(self.K+0.0001), 3)} \n',
+                    f'TOTAL LOSS \t train {round(total_loss, 3)} valid {round(total_loss_val, 3)} \n',
+                )
+
+            if  total_loss_val < best_loss:
+                best_loss = total_loss_val
+                best_model=copy.deepcopy(self.model)
+                
+            epoch_number += 1
+            self.scheduler.step()
+        
+        if self.bm_stop:
+            return best_model
+        else:
+            return self.model
